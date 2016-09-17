6eda3e4343f81c8e4fda317c3a9d7efde2b6f1e2
Henrique Taunay <htaunay@gmail.com>
2016-09-17 17:52:24 -0300

    Tests for Query Executor and Test Builder

---
+++ test/query-executor.js
@@ -0,0 +1,32 @@
+require("should");
+var fs = require('fs');
+var checkDom = require("../lib/dom-checker");
+var executeQuery = require("../lib/query-executor");
+
+describe("The query-tester module", function() {
+
+    it("should return an expected body for a given query obj", function(done) {
+    
+        var queryObj = {
+            "mkt": "pt-BR",
+            "query": "test"
+        };
+
+        executeQuery(queryObj, function(err, htmlBody) {
+           
+            var testObj = {
+                "path": "input#sb_form_q",
+                "attribute": "value",
+                "condition": "equals",
+                "expectedValue": "test"
+            };
+
+            checkDom(htmlBody, testObj, function(error, resultObj) {
+
+                resultObj["passed"].should.be.True();
+                done();
+            });
+        });
+    });
+
+});
---
+++ test/test-builder.js
@@ -0,0 +1,47 @@
+require("should");
+var buildTest = require("../lib/test-builder");
+
+describe("The test-builder module", function() {
+
+    it("should build a correct test script from an test spec tree", function(done) {
+
+        var testSpec = {
+
+            "name": "TestSpecName",
+            "testRoot": {
+               "mkt": "pt-BR",
+                "_children_" : [
+                {
+                    "q": "X",
+                    "_children_": [{"test": "123"}, {"something": 456}]
+                },
+                {
+                    "q": "Y",
+                    "_children_": [{"test": 321.0}, {"else": null}]
+                }
+                ]
+            }
+        };
+
+        buildTest(testSpec, function(err, testArray) {
+
+            (err == null).should.be.True();
+            (testArray instanceof Array).should.be.True();  
+
+            testArray.should.containEql(
+                { name: 'TestSpecName', mkt: 'pt-BR', q: 'X', test: '123' }
+            );
+            testArray.should.containEql(
+                { name: 'TestSpecName', mkt: 'pt-BR', q: 'X', something: 456 }
+            );
+            testArray.should.containEql(
+                { name: 'TestSpecName', mkt: 'pt-BR', q: 'Y', test: 321.0 }
+            );
+            testArray.should.containEql(
+                { name: 'TestSpecName', mkt: 'pt-BR', q: 'Y', else: null }
+            );
+
+            done();
+        });
+    });
+});
