3ee33c67b7ae828038541f01af1f7333d38fdc59
Henrique Taunay <henrique@taunay.me>
2017-03-06 14:12:44 +0000

    Missing test file

---
+++ test/testInput.js
@@ -0,0 +1,17 @@
+var test = {
+    "name": "FileInput",
+    "testRoot": {
+        "searchEngine": "bing",
+        "mkt": "pt-BR",
+        "query": "textinput",
+        "path": "input#sb_form_q",
+        "attribute": "value",
+        "condition": "contains",
+        "_children_": [
+            {"expectedValue": "tinp"},
+            {"expectedValue": "tiinp"}
+        ]
+    }
+};
+
+module.exports = test;
